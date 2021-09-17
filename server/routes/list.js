416151ee6f25e94a883b1a5ef82339dc1fe55993
Wa-ran <mgs.gilles@gmail.com>
2021-09-17 23:52:07 +0200

    au fait, tags v1 !

--- server/routes/list.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/list');
-
-router.get('/:fctName/:user_id', ctrl);
-
-router.post('/:fctName', ctrl);
-
-module.exports = router;
